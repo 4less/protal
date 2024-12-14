883d006088e0b92a57ecb7c59f124b7350c710ec
4less <joachim.fritscher@gmail.com>
2024-12-14 11:36:56 +0100

    Have binary with or without avx2 support, add protal_launcher to choose based on the system its running on

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+echo $PWD
+ls $PWD
+
 mkdir build
 cd build
 
 
 echo "Step 1"
+echo $PWD
+
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -21,5 +26,8 @@ echo $PWD
 echo "cmake --build cmake-build-release --target protal -- -j 6"
 
 cmake --build cmake-build-release --target protal -- -j 6
+cmake --build cmake-build-release --target protal_avx2 -- -j 6
 
-cp cmake-build-release/protal ${PREFIX}/bin
+cp cmake-build-release/protal ${PREFIX}/bin/protal_plain
+cp cmake-build-release/protal_avx2 ${PREFIX}/bin/protal_avx2
+cp ./../protal_launcher ${PREFIX}/bin/protal
