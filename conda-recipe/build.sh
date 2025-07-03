e0e94250fd520d3c9a8fde73956dd95f75f552c5
4less <joachim.fritscher@gmail.com>
2025-07-03 17:47:15 +0200

    fix build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,13 +2,16 @@
 echo $PWD
 ls $PWD
 
-mkdir build
-cd build
+# mkdir build
+# cd build
 
+rm -rf cmake-build-release
+mkdir -p cmake-build-release
 
 echo "Step 1"
 echo $PWD
-
+echo "...SRCDIR= $SRC_DIR"
+export VERBOSE=1
 
 cmake \
 	-DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -17,9 +20,12 @@ cmake \
 	-DCMAKE_C_COMPILER=$GCC \
 	-DCMAKE_CXX_COMPILER=$GXX \
 	-G "CodeBlocks - Unix Makefiles" \
-	-S ./../ \
+	-S $SRC_DIR \
 	-B cmake-build-release \
-        -DCMAKE_VERBOSE_MAKEFILE=ON
+    -DCMAKE_VERBOSE_MAKEFILE=ON
+
+
+	# -S ./../ \
 
 echo "Step 2"
 echo $PWD
@@ -30,4 +36,4 @@ cmake --build cmake-build-release --target protal_avx2 -- -j 6
 
 cp cmake-build-release/protal ${PREFIX}/bin/protal_plain
 cp cmake-build-release/protal_avx2 ${PREFIX}/bin/protal_avx2
-cp ./../protal_launcher ${PREFIX}/bin/protal
+cp protal_launcher ${PREFIX}/bin/protal
