db66c59591d153883b712e15fc1e4d3f5616d86b
4less <joachim.fritscher@gmail.com>
2025-10-21 14:40:07 +0200

    WIP patches

---
+++ src/Utilities/Benchmark.h
@@ -0,0 +1,113 @@
+//
+// Created by joachim on 05/06/2020.
+//
+
+#pragma once
+
+#include <string>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+using namespace std;
+
+namespace protal {
+    enum Time {
+        days, hours, minutes, seconds, milliseconds, microseconds
+    };
+
+    class Benchmark {
+        string name;
+        long time_sum = 0;
+        size_t samplings = 0;
+        size_t threads_sampled = 0;
+        time_point<high_resolution_clock> start_time;
+
+    public:
+        Benchmark(string name, size_t threads_sampled=0) : name(name), threads_sampled(threads_sampled) {
+            //Start();
+        }
+
+        std::string GetName() const {
+            return name;
+        }
+
+        void AddObservation() {
+            samplings++;
+        }
+
+        void Start(bool new_sample=true) {
+            samplings += new_sample;
+            start_time = high_resolution_clock::now();
+        }
+
+        void Stop() {
+//            if (time_sum != 0) return;
+            auto stop_time = high_resolution_clock::now();
+            auto duration = duration_cast<std::chrono::microseconds>(stop_time - start_time);
+//            std::cout << "Duration count : " << duration.count() << std::endl;
+            time_sum += duration.count();
+        }
+
+        uint64_t GetDuration(Time format) {
+//            if (time_sum == 0) {
+//                Stop();
+//            }
+
+            switch (format) {
+                case Time::microseconds:
+                    return time_sum;
+                case Time::milliseconds:
+                    return time_sum / 1'000;
+                case Time::seconds:
+                    return time_sum / 1'000'000;
+                case Time::minutes:
+                    return time_sum / 60'000'000;
+                case Time::hours:
+                    return time_sum / 3'600'000'000;
+                case Time::days:
+                    return time_sum / 86'400'000'000;
+            }
+            return 0L;
+        }
+
+        void Join(Benchmark const& other, bool add_thread=true) {
+            if (add_thread && other.samplings > 0) threads_sampled++;
+            time_sum += other.time_sum;
+        }
+
+        void PrintResults() {
+            if (time_sum == 0) {
+                Stop();
+            }
+
+            if (samplings > 0) {
+                threads_sampled++;
+            }
+
+            if (threads_sampled > 1) {
+                time_sum /= static_cast<double>(threads_sampled);
+            }
+
+            auto time_sum_local = time_sum;
+
+            uint hours = time_sum_local / 3600000000;
+            time_sum_local -= hours * 3600000000;
+            uint mins = time_sum_local / 60000000;
+            time_sum_local -= mins * 60000000;
+            uint secs = time_sum_local / 1000000;
+            time_sum_local -= secs * 1000000;
+            uint msecs = time_sum_local / 1000;
+
+            std::cout << name << " took ";
+            if (hours) std::cout << hours << "h ";
+            if (mins) std::cout << mins << "m ";
+            if (secs) std::cout << secs << "s ";
+            if (msecs) std::cout << msecs << "ms";
+            if (!hours && !mins && !secs && !msecs) std::cout << " less than 0ms";
+            if (samplings > 1) std::cout << " (" << std::to_string(samplings) << ")";
+            if (threads_sampled > 1) std::cout << " mean over " << threads_sampled << " threads";
+            std::cout << std::endl;
+        }
+    };
+}
---
+++ src/Utilities/BinaryClassifierEvaluator.h
@@ -0,0 +1,87 @@
+//
+// Created by fritsche on 03/02/2022.
+//
+
+#pragma once
+
+#include <cstddef>
+#include <ostream>
+#include <iostream>
+
+struct BinaryClassifierEvaluator {
+    std::string name {};
+
+    size_t tp = 0;
+    size_t tn = 0;
+    size_t fp = 0;
+    size_t fn = 0;
+
+    BinaryClassifierEvaluator() = default;
+    explicit BinaryClassifierEvaluator(std::string name) :
+        name(name) {};
+
+    void SetName(std::string name) {
+        this->name = name;
+    }
+
+    double Sensitivity() const {
+        if (tp + fn == 0) return -1;
+        return (double) tp/static_cast<double>(tp + fn);
+    }
+
+    double Specificity() const {
+        if (tn + fp == 0) return -1;
+        return (double) tn/static_cast<double>(tn + fp);
+    }
+
+    double Precision() const {
+        if (tp + fp == 0) return -1;
+        return (double) tp/static_cast<double>(tp + fp);
+    }
+
+    double Accuracy() const {
+        if (tp + fp + tn + fn == 0) return -1;
+        return (double) (tp + tn)/static_cast<double>(tp + tn + fp + fn);
+    }
+
+    double F1() const {
+        if (tp + fp + tn + fn == 0) return -1;
+        return (double) 2*tp/static_cast<double>(2 * tp + fp + fn);
+    }
+
+
+    void WriteStats(std::ostream &os=std::cout) const {
+        os << "Evaluate binary classifier: " << std::endl;
+        os << "tp: " << tp << " fp: " << fp << " tn: " << tn << " fn: " << fn << std::endl;
+        os << "Sensitivity:\t\t" << Sensitivity() << std::endl;
+        os << "Specificity:\t\t" << Specificity() << std::endl;
+        os << "Precision:  \t\t" << Precision() << std::endl;
+        os << "Accuracy:   \t\t" << Accuracy() << std::endl;
+        os << "F1:         \t\t" << F1() << std::endl;
+    }
+
+    static void WriteRowHeader(std::ostream &os=std::cout, std::string row_prefix="") {
+        os << row_prefix << "Name\tTP\tFP\tTN\tFN\tSensitivity\tSpecificity\tPrecision\tAccuracy\tF1" << std::endl;
+    }
+
+    void Join(BinaryClassifierEvaluator const& other) {
+        this->tp += other.tp;
+        this->tn += other.tn;
+        this->fp += other.fp;
+        this->fn += other.fn;
+    }
+
+    void WriteRowStats(std::ostream &os=std::cout, std::string row_prefix="") const {
+        os << row_prefix;
+        os << name << '\t';
+        os << tp << '\t';
+        os << fp << '\t';
+        os << tn << '\t';
+        os << fn << '\t';
+        os << Sensitivity() << '\t';
+        os << Specificity() << '\t';
+        os << Precision() << '\t';
+        os << Accuracy() << '\t';
+        os << F1() << std::endl;
+    }
+};
