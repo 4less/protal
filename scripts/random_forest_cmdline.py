f9b5d406cca502c3c76fbf81014d6d2383995397
4less <joachim.fritscher@gmail.com>
2026-04-12 19:11:42 +0200

    Change to class weight balanced_subsample

--- scripts/random_forest_cmdline.py
+++ scripts/random_forest_cmdline.py
@@ -376,7 +376,7 @@ def main() -> int:
             n_jobs=opts.threads,
             random_state=opts.seed,
             max_leaf_nodes=opts.maxnodes if opts.maxnodes > 0 else None,
-            class_weight="balanced",
+            class_weight="balanced_subsample",
         )
         grid_search = GridSearchCV(
             estimator=base_model,
